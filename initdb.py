2c15482b00f2afb6fdf20c2e954780fafedc3ce2
lziad <23732689+lziad@users.noreply.github.com>
2019-11-08 20:11:30 +0800

    First commit

---
+++ initdb.py
@@ -0,0 +1,20 @@
+import sqlite3
+
+conn = sqlite3.connect('pages.db')
+c = conn.cursor()
+
+# entry table lists the current speedy deletion candidates
+c.execute('''CREATE TABLE entry
+            (id integer primary key, site text, siteurl text, title text,
+             requester text, bot integer, ts integer)''')
+# log table includes who requested for SD and who deleted a candidate, etc.
+# add: added to CSD category, e.g. added speedy templates
+# rm: removed from CSD category, e.g. removed speedy templates
+# del: deleted the related page
+c.execute('''CREATE TABLE log
+            (id integer primary key, site text, siteurl text, title text,
+             user text, type text check( type in ('add', 'rm', 'del') ),
+             bot integer, ts integer)''')
+
+conn.commit()
+conn.close()
---
+++ update.py
@@ -0,0 +1,87 @@
+import re
+import json
+import sqlite3
+from enums import CSDCATS, RMCATCMT, ADDCATCMT
+from sseclient import SSEClient as EventSource
+from time import sleep
+
+conn = sqlite3.connect('pages.db')
+cur = conn.cursor()
+
+# get title from the summaries like "[[:title]] removed from category"
+add_title_re = re.compile(r'|'.join([s.replace('[[:$1]]', r'\[\[:(.+)\]\]')
+    for s in ADDCATCMT.values()]))
+rm_title_re = re.compile(r'|'.join([s.replace('[[:$1]]', r'\[\[:(.+)\]\]')
+    for s in RMCATCMT.values()]))
+
+url = 'https://stream.wikimedia.org/v2/stream/recentchange'
+for event in EventSource(url):
+    if event.event == 'message':
+        try:
+            change = json.loads(event.data)
+
+            if change['type'] == 'categorize':
+                if change['title'] != CSDCATS.get(change['wiki']):
+                    continue
+                add_match = add_title_re.search(change['comment'])
+                title = None
+                if add_match:
+                    title = next(t for t in add_match.groups() if t)
+                    print('added', change['wiki'], title)
+                    cur.execute('''INSERT INTO entry
+                        (site, siteurl, title, requester, bot, ts) VALUES
+                        (?,?,?,?,?,?)''',
+                        (change['wiki'], change['server_url'], title,
+                        change['user'], change['bot'], change['timestamp']))
+                    cur.execute('''INSERT INTO log
+                        (site, siteurl, title, user, type, bot, ts) VALUES
+                        (?,?,?,?,?,?,?)''',
+                        (change['wiki'], change['server_url'], title,
+                        change['user'], 'add', change['bot'],
+                        change['timestamp']))
+                else:
+                    rm_match = rm_title_re.search(change['comment'])
+                    title = next(t for t in rm_match.groups() if t)
+                    cur.execute("SELECT * FROM entry WHERE title = ?",
+                        (title,))
+                    if cur.fetchone():
+                        print('removed', change['wiki'], title)
+                        cur.execute("DELETE FROM entry WHERE title = ?",
+                            (title,))
+                        cur.execute('''INSERT INTO log
+                        (site, siteurl, title, user, type, bot, ts) VALUES
+                        (?,?,?,?,?,?,?)''',
+                        (change['wiki'], change['server_url'], title,
+                        change['user'], 'rm', change['bot'],
+                        change['timestamp']))
+
+            elif change['type'] == 'log':
+                if change['log_type'] != 'delete':
+                    continue
+                cur.execute("SELECT * FROM entry WHERE title = ?",
+                    (change['title'],))
+                if cur.fetchone():
+                    print('deleted', change['wiki'], change['title'])
+                    cur.execute("DELETE FROM entry WHERE title = ?", (title,))
+                    cur.execute('''INSERT INTO log
+                        (site, siteurl, title, user, type, bot, ts) VALUES
+                        (?,?,?,?,?,?,?)''',
+                    (change['wiki'], change['server_url'], change['title'],
+                    change['user'], 'del', change['bot'],
+                    change['timestamp']))
+        except (ValueError, KeyError):
+            # change parse error
+            pass
+        except AttributeError:
+            # summary parse error
+            pass
+        else:
+            pass
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
