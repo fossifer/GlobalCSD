e0a95ab15666e720b15334dfc45c34a664e377ab
lziad <23732689+lziad@users.noreply.github.com>
2019-12-01 22:05:44 +0800

    rewrite for django

--- init.py
+++ init.py
@@ -1,55 +1,61 @@
 import sqlite3
 import toolforge
-from enum import CSDCATS
+from time import time
+from enums import CSDCATS
+from datetime import datetime as dt
 
 toolforge.set_user_agent('GlobalCSD/0.1 (https://tools.wmflabs.org/globalcsd/; https://meta.wikimedia.org/wiki/User_talk:WhitePhosphorus) Python/3.5 init.py/0.1')
 
-local_conn = sqlite3.connect('pages.db')
+local_conn = sqlite3.connect('db.sqlite3', timeout=60)
 local_cur = local_conn.cursor()
 
-for wiki in CSDCATS.keys():
+# Fetch admins of each site
+local_cur.execute('''SELECT name FROM showcsd_wiki''')
+wikis = local_cur.fetchall()
+local_cur.execute('''SELECT name FROM showcsd_admin JOIN showcsd_wiki ON site_id = showcsd_wiki.id GROUP BY site_id''')
+exclude_wikis = local_cur.fetchall()
+wikis = set([w[0] for w in wikis]) - set([w[0] for w in exclude_wikis])
+print(len(wikis), 'wikis')
+for wiki in wikis:
+    print(wiki)
     conn = toolforge.connect(wiki)
     with conn.cursor() as cur:
-        # admin info
-        cur.execute('''SELECT user_id, user_name, ug_expiry
+        starttime = time()
+        cur.execute('''SELECT user_id, user_name, UNIX_TIMESTAMP(ug_expiry)
             FROM user_groups JOIN user ON ug_user = user_id
             WHERE ug_group LIKE "%sysop%";''')
         admins = cur.fetchall()
-        admin_ids = (a[0] for a in admins)
-        admins = {a[0]: (a[1], a[2]) for a in admins}
-        # get last edit time
-        # https://stackoverflow.com/a/28090544
-        cur.execute('''SELECT ra.actor_user, r.rev_timestamp
-            FROM revision r
-            JOIN revision_actor_temp rat ON rat.revactor_rev = r.rev_id
-            JOIN actor ra ON rat.revactor_actor = ra.actor_id
-            LEFT JOIN revision b
-            JOIN revision_actor_temp bat ON bat.revactor_rev = b.rev_id
-            JOIN actor ba ON bat.revactor_actor = ba.actor_id
-            ON ra.actor_user = ba.actor_user
-                AND r.rev_timestamp < b.rev_timestamp
-            WHERE ra.actor_user IN (%s) AND b.rev_timestamp IS NULL
-            ''' % ','.join(['%s'] * len(admins)), admin_ids)
-        last_edit_time = dict(cur.fetchall())
-        # get last log time
-        cur.execute('''SELECT ra.actor_user, r.log_timestamp
-            FROM logging r
-            JOIN actor ra ON r.log_actor = ra.actor_id
-            LEFT JOIN logging b
-            JOIN actor ba ON b.log_actor = ba.actor_id
-            ON ra.actor_user = ba.actor_user
-                AND r.log_timestamp < b.log_timestamp
-            WHERE ra.actor_user IN (%s) AND b.log_timestamp IS NULL
-            ''' % ','.join(['%s'] * len(admins)), admin_ids)
-        last_log_time = dict(cur.fetchall())
-        # get maximum of both timestamps,
-        # and construct the tuple to insert
-        rst = ((wiki,
-                a,
-                admins[a][0],
-                admins[a][1],
-                max(last_edit_time.get(a, 0), last_log_time.get(a, 0)))
-            for a in admin_ids)
-        local_cur.executemany('''INSERT INTO admin
-            (site, userid, username, expiry, action) VALUES (?,?,?,?,?)''',
+        admin_ids = tuple(a[0] for a in admins)
+        cur.execute('''SELECT actor_id FROM actor WHERE actor_user
+            IN (%s)''' % ','.join(['%s'] * len(admins)), admin_ids)
+        actor_ids = cur.fetchall()
+        actor_ids = tuple(a[0] for a in actor_ids)
+        admins_d = {actor_ids[i]: admins[i] for i in range(len(admins))}
+        print(len(actor_ids), 'admins')
+        cur.execute('''SELECT rev_actor, UNIX_TIMESTAMP(MAX(rev_timestamp))
+            FROM revision_userindex WHERE rev_actor
+            IN (%s) GROUP BY rev_actor''' % ','.join(['%s'] * len(admins)),
+            actor_ids)
+        last_edit_time = cur.fetchall()
+        cur.execute('''SELECT log_actor, UNIX_TIMESTAMP(MAX(log_timestamp))
+            FROM logging_userindex WHERE log_actor
+            IN (%s) GROUP BY log_actor''' % ','.join(['%s'] * len(admins)),
+            actor_ids)
+        last_log_time = cur.fetchall()
+        last_edit_time_dict = {e[0]: e[1] for e in last_edit_time}
+        last_log_time_dict = {e[0]: e[1] for e in last_log_time}
+        last_action_time_dict = {k: max(
+            last_edit_time_dict.get(k, -1), last_log_time_dict.get(k, -1))
+            for k in admins_d.keys()}
+        rst = tuple((wiki, admins_d[k][0], admins_d[k][1].decode('utf-8'),
+                dt.utcfromtimestamp(admins_d[k][2]) \
+                    if admins_d[k][2] else None,
+                dt.utcfromtimestamp(last_action_time_dict[k]) \
+                    if last_action_time_dict[k] != -1 else None)
+            for k in admins_d.keys())
+        print('query time:', time()-starttime)
+        local_cur.executemany('''INSERT INTO showcsd_admin
+            (site_id, userid, username, expiry, action)
+            VALUES ((SELECT id FROM showcsd_wiki WHERE name = ?),?,?,?,?)''',
             rst)
+        local_conn.commit()
